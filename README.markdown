8bde0b17232f055311f7a02b11f2b80df9c19db7
Jonas De Smet <jonas@glamorous.be>
2010-04-07 22:58:36 +0200

    First commit

---
+++ README.markdown
@@ -0,0 +1,103 @@
+## jQuery Utilities plugin ##
+
+### Why this plugin ###
+
+This plugin is for everyone who had the same problem like me. I hate it to write the same code over and over. This plugin does some basic stuff so I don't need to write the code for every project.
+
+### What does it do? ###
+
+* Search all external links and adds an attribute `target` with value `_blank` and a class 'external'.
+* Array.unique() functionality
+* ScrollTo
+* Logging to console or alert
+* Add a rule-method to the jQuery Validate plugin for defaultValue
+* Functionality for defaultValue in text-inputs to clear and come back when an input get focused or blurred.
+
+### Getting Started ###
+
+To use the Utilities plugin, you have to include the jQuery library in your `<head>` tag of your HTML document or just above the closing `<body>`-tag.
+
+    <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.4.2/jquery.min.js"></script>
+    <script type="text/javascript" src="/js/jquery.glam.utilities.js"></script>
+    <script type="text/javascript" src="/js/general.js"></script>
+    </body>
+    </html>
+
+We will use the `general.js`-file for the specefic jQuery-code to call our plugin.
+
+### Basic setup ###
+
+    $(document).ready(function(){
+        $.glamUtilities();
+    });
+
+Yes, it's so simple! The code above is pretty the same as this:
+
+    $(document).ready(function(){
+        $.glamUtilities({
+            debug: false,
+    		externalLinks: true,
+    		externalClass: 'external',
+    		defaultValueAsLabel: false,
+    		validateDefaultValue: false
+        });
+    });
+    
+### More advanced setup ###
+    
+    $(document).ready(function(){
+        $.glamUtilities({
+            debug: false,
+    		externalLinks: true,
+    		externalClass: 'otherNameForClassAtAllExternalLinks',
+    		defaultValueAsLabel: true, // or you can specify wich elements that has to change: $("form input[type=text]")
+    		validateDefaultValue: false
+        });
+    });
+    
+## If you use the jQuery Validate plugin than this is handy ##
+
+    $(document).ready(function(){
+        $.glamUtilities({
+      		validateDefaultValue: {
+      		    name: 'RuleName',
+      		    error: 'This is the error message for the rule'
+      		}
+        });
+    });
+    
+## How to log with the plugin ##
+
+    $.glamLog('This is my message','DEBUG'); // DEBUG is the log-type, other: INFO, WARN, ERROR, LOG (default-type)
+    
+## How to use the ScrollTo function with the plugin ##
+
+    $.glamScroll($("#top"), 200); //default 500ms
+    
+## How to use the Array unique function ##
+
+    var myArray = [1,3,4,2,2,4];
+    
+    myArray.unique();
+    
+    // myArray is now [1,3,4,2]
+
+## Issues/Bugs ##
+
+If you find one, please inform us with the issue tracker on [github](http://github.com/glamorous/jQuery-Utilities/issues).
+
+## Changelog ##
+
+**0.1 - 07/04/2010**
+
+- First release
+
+## Feature Requests / To come ##
+
+*  Minified and packed versions of the plugin
+
+If you want something to add on this plugin, feel free to fork the project on [github](http://github.com/glamorous/jQuery-Utilities) or add an [issue](http://github.com/glamorous/jQuery-Utilities/issues) as a feature request.
+
+## License ##
+
+This plugin has a [BSD License](http://www.opensource.org/licenses/bsd-license.php). You can find the license in license.txt that is included with plugin-package
---
+++ jquery.glam.utilities.js
@@ -0,0 +1,147 @@
+/**
+ * Glamorous Utilities - Some cool additions to Javascript and handy functions
+ * Documentation and usage in README file
+ * 
+ * @author Jonas De Smet - Glamorous
+ * @since 07.04.2010
+ * @copyright Jonas De Smet - Glamorous
+ * @version 0.1
+ * @license BSD http://www.opensource.org/licenses/bsd-license.php
+ * 
+ */
+(function($) {
+	// Set the utilities plugin
+	$.glamUtilities = function(settings)
+	{
+		settings = $.extend({}, $.glamUtilities.defaults, settings);
+		
+		// Method to search for all links and add a target='_blank' to it
+		function changeTargets(class)
+		{
+			var links = $('a');
+			var l_length = links.length;
+			var external_links = []; 
+			for (var i=0;i<l_length;i++)
+			{
+				var href = $(links[i]).attr('href');
+				
+				if (href !== undefined && href.match(/^https?\:/i) && !href.match(document.domain)) 
+				{
+					external_links.push(links[i]);
+				}
+			}
+
+			$(external_links).attr("target", "_blank").addClass(class);
+		}
+		
+		// Method to set defaultValue as a inside label for text inputs
+		function setInsideLabels(text_inputs)
+		{
+			var $inputs = (text_inputs === true) ? $("form input[type=text]") : text_inputs;
+			
+			$inputs.each(function(){
+				var input = $(this);
+				input.click(function(){
+					if(input.val() == this.defaultValue){
+						input.val("");
+					}
+				});
+				input.blur(function(){
+					if(input.val() == ""){
+						input.val(this.defaultValue);
+					}
+				});
+			});
+
+		}
+		
+		// Method to extend the jQuery Validate plugin with a rule for defaultValue
+		function setValidateDefault(settings)
+		{
+			// Test if validator is not undefined
+			if(jQuery.validator !== undefined)
+			{
+				jQuery.validator.addMethod(settings.name, function(value, element) { 
+					return this.optional(element) || element.defaultValue != value; 
+				}, settings.error);
+			}
+		}
+		
+		// Look wich custom methods has to be called
+		function init()
+		{	
+			if(settings.externalLinks === true){changeTargets(settings.externalClass);}
+			if(settings.defaultValueAsLabel !== false){setInsideLabels(settings.defaultValueAsLabel);}
+			if(settings.validateDefaultValue !== false){setValidateDefault(settings.validateDefaultValue);}
+		}
+		
+		init();
+	};
+
+	// Default settings for Utilities plugin
+	$.glamUtilities.defaults = 
+	{
+		debug: false,
+		externalLinks: true,
+		externalClass: 'external',
+		defaultValueAsLabel: false,
+		validateDefaultValue: false
+	};
+
+	// Glamorous SCROLLING
+	$.glamScroll = function(selector, speed)
+	{
+		console.log(speed);
+		speed = (speed !== undefined) ? speed : 500;
+		$('html,body').animate({
+			scrollTop: $(selector).offset().top
+		}, speed);
+	};
+
+	// Glamorous LOGGING
+	$.glamLog = function(message, type, plugin) 
+	{
+		var msg = '['+type+'] '+plugin+' - '+message;
+		if(window.console !== undefined) {
+			switch(type)
+			{
+				case 'DEBUG':
+					console.debug(msg);
+					break;
+				case 'INFO':
+					console.info(msg);
+					break;
+				case 'WARN':
+					console.warn(msg);
+					break;
+				case 'ERROR':
+					console.error(msg);
+					break;
+                default:
+                case 'LOG':
+					console.log(msg);
+					break;
+			}
+		} else {
+			alert(msg);
+		}
+	};
+	
+	// Extend Array to have a "unique"-function to remove duplicates
+	Array.prototype.unique = function()
+	{
+		var arr = [];
+		loop:for(var i=0, m=this.length; i<m; i++)
+		{
+			for(var j=0, n=arr.length; j<n; j++)
+			{
+				if(arr[j]==this[i])
+				{
+					continue loop;
+				}
+			}
+			arr[arr.length] = this[i];
+		}
+		return arr;
+	};
+})(jQuery);
